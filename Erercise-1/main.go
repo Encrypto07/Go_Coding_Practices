477ed928cfd5d5db2e05cbb3fdb54660ae360a8e
sultan <sultan8512@gmail.com>
2021-06-16 17:46:33 +0530

    Exercise First

--- Erercise-1/main.go
+++ Erercise-1/main.go
@@ -1,3 +1,11 @@
+/*Hands-on exercise #1
+Using the short declaration operator, ASSIGN these VALUES to VARIABLES with the IDENTIFIERS “x” and “y” and “z”
+42 “James Bond” true
+Now print the values stored in those variables using
+a single print statement
+b multiple print statements
+*/
+
 package main
 
 import (
@@ -5,5 +13,16 @@ import (
 )
 
 func main() {
-	fmt.Println()
+
+	x := 42
+	y := "James Bond"
+	z := true
+
+	//Printing on a single Line
+	fmt.Println(x, y, z)
+
+	//Printing On Multiple line
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
 }
